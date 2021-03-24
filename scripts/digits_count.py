4776b88f8a026884e57976b8a2f63e92f7ef1cdc
aleksei_mahonin <sharkizz@mail.ru>
2021-03-25 00:48:49 +0300

    Init

--- scripts/digits_count.py
+++
@@ -1,9 +0,0 @@
-# If input is int
-def digits_counter(number_to_cnt_digs, cnt=0):
-    if number_to_cnt_digs == 0:
-        return cnt
-    else:
-        return digits_counter(number_to_cnt_digs // 10, cnt + 1)
-
-# Also use
-# return len(str(number_to_cnt_digs))
